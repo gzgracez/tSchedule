a3665745a674c91285ed476eb669a6a7c04ed7a0
Grace Z <gz.gracez16@gmail.com>
2015-09-01 23:24:53 -0400

    times

--- sched.cpp
+++ sched.cpp
@@ -8,16 +8,30 @@ int main()
 {
   //ostringstream ss;
   string colors[6][8];
-  ifstream readFile;
-  readFile.open("colors.txt");
+  ifstream readColorFile;
+  readColorFile.open("colors.txt");
   int count = 0;
-  while(!readFile.eof() && count/8<6)
+  while(!readColorFile.eof() && count/8<6)
     {
-      getline(readFile, colors[count/8][count%8]);
+      getline(readColorFile, colors[count/8][count%8]);
       cout << count/8 << ", " << count%8 << ": " << colors[count/8][count%8] << "\n" <<endl;
       count++;
     }
-  readFile.close();
+  readColorFile.close();
+
+  //times
+  string times[6][8];
+  ifstream readTimeFile;
+  readTimeFile.open("times.txt");
+  int tCount = 0;
+  while(! readTimeFile.eof() && tCount/8<6)
+    {
+      getline(readTimeFile, times[tCount/8][tCount%8]);
+      cout << tCount/8 << ", " << tCount%8 << ": " << times[tCount/8][tCount%8] << "\n" <<endl;
+      tCount++;
+    }
+  readTimeFile.close();
+
   cout << "End\n"<<endl;
   
   return 0;
