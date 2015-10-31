5b383d188f36b2a6ec4eca56918c728bfb36f190
Grace Zhang <gz.gracez16@gmail.com>
2015-10-30 21:06:01 -0400

    tomorrow & tom add # args for future dates

--- sched.cpp
+++ sched.cpp
@@ -177,7 +177,7 @@ int main(int argc, char *argv[])
     printSchedule(today);
   }
   else if (argc == 2) {
-    if (strcmp(argv[1], "tomorrow") == 0) {
+    if (strcmp(argv[1], "tomorrow") == 0 || strcmp(argv[1], "tom") == 0) {
       printSchedule(tomorrow);
     }
     else {
